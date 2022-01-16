de44e2cc83b130b451f5162c52a1a940992f5108
Jorge Hernandez <3452489+jhernandezb@users.noreply.github.com>
2022-01-15 22:46:01 -0600

    update config (#8)

--- src/config.js
+++ src/config.js
@@ -16,5 +16,5 @@ export const config = {
     GAS_PRICE_STEP_LOW: 0.0,
     GAS_PRICE_STEP_AVERAGE: 0.0,
     GAS_PRICE_STEP_HIGH: 0.025,
-    FEATURES: ['stargate', 'ibc-transfer'],
+    FEATURES: ['stargate', 'ibc-transfer', 'no-legacy-stdTx'],
 };
